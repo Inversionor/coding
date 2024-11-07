d769448a5a16d397b1fae5c1b8f2df03a86c06f3
linzhujie <932874230@qq.com>
2024-11-07 20:21:33 +0800

    分发饼干

--- src/main/java/com/perfectcode/aida2/debug_test/LeetCode/LC455.java
+++ src/main/java/com/perfectcode/aida2/debug_test/LeetCode/LC455.java
@@ -1,8 +1,22 @@
 package com.perfectcode.aida2.debug_test.LeetCode;
 
+import java.util.Arrays;
+
 public class LC455 {
     public int findContentChildren(int[] g, int[] s) {
-        return 0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int count = 0;
+        int start = s.length - 1;
+        // 遍历胃口
+        for (int index = g.length - 1; index >= 0; index--) {
+            if(start >= 0 && g[index] <= s[start]) {
+                start--;
+                count++;
+            }
+        }
+        return count;
+
 
     }
 }
