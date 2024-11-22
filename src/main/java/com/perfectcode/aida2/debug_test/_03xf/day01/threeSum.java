a3a963e37b6998fc84d276e330815e93dcc1f838
林诸杰 <linzhujie@192.168.124.10>
2024-11-23 00:02:54 +0800

    lingshentidan

--- src/main/java/com/perfectcode/aida2/debug_test/_03xf/day01/threeSum.java
+++ src/main/java/com/perfectcode/aida2/debug_test/_03xf/day01/threeSum.java
@@ -1,4 +1,66 @@
 package com.perfectcode.aida2.debug_test._03xf.day01;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 三数之和
+ * 特点：通过对数组排序后，三数之和a+b+c=0可以转换成a+b=-c的问题，可以使用双指针。
+ * api：Arraus.asList()
+ *
+ */
+
 public class threeSum {
+    public List<List<Integer>>  threeSum(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int len=nums.length;
+        int left = 0;
+
+        for(;left<len-2;left++){
+            //剪枝
+            if(nums[left]>0){
+                break;
+            }
+            //去重
+            if(left>0&&nums[left]==nums[left-1]){
+                continue;
+            }
+            //每一次计算都要从两端开始
+            int mid=left+1;
+            int right = nums.length-1;
+            while(mid<right){
+                int sum=nums[left]+nums[mid]+nums[right];
+                if(sum==0){
+                    res.add(Arrays.asList(nums[left], nums[mid], nums[right]));
+                    //去重
+                    while(mid<right&&nums[mid]==nums[++mid]){
+                    }
+                    while(mid<right&&nums[right]==nums[--right]){
+                    }
+                }else if(sum<0){
+                    //去重
+                    while (mid<right&&nums[mid]==nums[++mid]){
+                    }
+                }else{
+                    //去重
+                    while(mid<right&&nums[right]==nums[--right]){
+                    }
+                }
+            }
+        }
+        return res;
+    }
+    public static void main(String[] args) {
+        threeSum threeSum = new threeSum();
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> lists = threeSum.threeSum(nums);
+        for (List<Integer> list : lists){
+            for (Integer integer : list){
+                System.out.print(integer+" ");
+            }
+        }
+
+    }
 }
--- src/main/java/com/perfectcode/aida2/debug_test/_03xf/day01/twoSum.java
+++ src/main/java/com/perfectcode/aida2/debug_test/_03xf/day01/twoSum.java
@@ -1,4 +1,24 @@
 package com.perfectcode.aida2.debug_test._03xf.day01;
 
+/**
+ * 两数之和2️⃣-输入有序数组
+ * 特点：数组已经排好序
+ */
 public class twoSum {
+    public int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right = numbers.length-1;
+        while(left<right){
+            if (numbers[left]+numbers[right]>target){
+                right--;
+            }
+            else if(numbers[left]+numbers[right]<target){
+                left++;
+            }
+            else{
+                return new int[]{left+1,right+1};
+            }
+        }
+        return new int[]{-1,-1};
+    }
 }
