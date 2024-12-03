5976572d3f849c56648d4a6a55cb23f595bc3d43
linzhujie <932874230@qq.com>
2024-12-04 02:29:24 +0800

    tijiao

--- src/main/java/com/perfectcode/aida2/debug_test/_03xf/day03/longestSemiRepetitiveSubstring.java
+++ src/main/java/com/perfectcode/aida2/debug_test/_03xf/day03/longestSemiRepetitiveSubstring.java
@@ -1,4 +1,11 @@
 package com.perfectcode.aida2.debug_test._03xf.day03;
 
+/**
+ * 2730.找到最长的半重复字符串
+ */
+
 public class longestSemiRepetitiveSubstring {
+    public int longestSemiRepetitiveSubstring(String s) {
+
+    }
 }
--- src/main/java/com/perfectcode/aida2/debug_test/_03xf/day03/maxSubarrayLength.java
+++ src/main/java/com/perfectcode/aida2/debug_test/_03xf/day03/maxSubarrayLength.java
@@ -1,4 +1,27 @@
 package com.perfectcode.aida2.debug_test._03xf.day03;
 
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 2958. 最多k个重复元素的最长子数组
+ */
 public class maxSubarrayLength {
+    public int maxSubarrayLength(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int res= Integer.MIN_VALUE;
+        int left=0;
+        for (int i = 0; i < nums.length; i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if (map.get(nums[i])<=k){
+                res=Math.max(res,i-left+1);
+            }
+            while (map.get(nums[i])>k){
+                map.put(nums[left],map.get(nums[left])-1);
+                left++;
+            }
+
+        }
+        return  res;
+    }
 }
